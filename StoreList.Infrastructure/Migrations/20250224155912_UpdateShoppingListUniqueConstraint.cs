ad780ee238453f8f0e5641d76ac89d29c834706c
Yannick <cleps2012@gmail.com>
2025-02-24 17:03:16 +0100

    Update ShoppingListUniqueConstraint

---
+++ StoreList.Infrastructure/Migrations/20250224155912_UpdateShoppingListUniqueConstraint.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreList.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateShoppingListUniqueConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ShoppingLists_Name",
+                table: "ShoppingLists");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingLists_Name_UserId",
+                table: "ShoppingLists",
+                columns: new[] { "Name", "UserId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ShoppingLists_Name_UserId",
+                table: "ShoppingLists");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingLists_Name",
+                table: "ShoppingLists",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
